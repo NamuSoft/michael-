2bd5ef31e3545f0b9234bffb7d5aa88251cda44e
MikeRock51 <mikerockmusic51@gmail.com>
2024-07-02 06:18:19 +0100

    chore: implement skill tag/add svgAtom

--- src/app/home/container/HomeContainer.tsx
+++ src/app/home/container/HomeContainer.tsx
@@ -2,7 +2,13 @@ import { HomeTemplate } from "~/components/Templates/Home/HomeTemplate";
 
 export const HomeContainer = () => {
   const homeTemplateProps: React.ComponentProps<typeof HomeTemplate> = {
-    homeContentModuleProps: { title: "HomeContentModule" },
+    homeContentModuleProps: {
+      skills: [
+        { name: "HTML5 & CSS3", level: 94, icon: "html5" },
+        { name: "Bootstrap", level: 98, icon: "bootstrap" },
+        { name: "SEO", level: 75, icon: "search" }
+      ],
+    },
   };
 
   return <HomeTemplate {...homeTemplateProps} />;
--- src/components/Modules/Home/HomeContentModule.tsx
+++ src/components/Modules/Home/HomeContentModule.tsx
@@ -1,13 +1,21 @@
 import { Image } from "antd";
+import React from "react";
 import Sidebar from "~/components/SideBar.tsx/Sidebar";
+import SkillTag from "~/components/SkillTag/SkillTag";
+
+export type Skill = {
+  name: string;
+  level: number;
+  icon: "html5" | "bootstrap" | "search";
+};
 
 type Props = {
-  title: string;
+  skills: Skill[];
 };
 
 export const HomeContentModule = (props: Props) => {
   return (
-    <div className='mx-auto max-w-[1400px] py-6'>
+    <div className='mx-auto max-w-[1300px] py-6'>
       <div className='mb-8 flex gap-4 text-7xl font-bold'>
         <h1>Christina</h1>
         <h1 className='font-outline-2 text-white'>Gray</h1>
@@ -16,27 +24,36 @@ export const HomeContentModule = (props: Props) => {
         <div className='col-span-1'>
           <Sidebar />
         </div>
-        <div className='col-span-3 rounded-2xl p-12 shadow'>
-          <div className='flex gap-12'>
+        <div className='col-span-3 rounded-2xl bg-white p-12 shadow'>
+          <div className='flex w-full gap-12'>
             <Image
               src='/images/hero-avatar.webp'
               alt='Hero Avatar'
               preview={false}
-              className='h-64 w-64 rounded-full'
+              className='h-64 min-w-64 rounded-full'
             />
-            <div className='flex flex-col gap-4'>
+            <div className='flex w-full flex-col gap-4'>
               <div className='relative'>
                 <h1 className='font-outline-2 text-5xl font-bold text-white opacity-10'>
                   ABOUT ME
                 </h1>
                 <p className='absolute bottom-0 left-1 font-medium'>
-                  // ABOUT ME
+                  {"// ABOUT ME"}
                 </p>
               </div>
               <div className='text-[40px] font-bold'>
                 <h2>UI & UX Designer.</h2>
-                <h2 className="-mt-2">Photographer</h2>
+                <h2 className='-mt-2'>Photographer</h2>
               </div>
+              <div className='flex items-center justify-between'>
+                {props.skills.map((skill, index) => (
+                  <SkillTag key={index} {...skill} />
+                ))}
+              </div>
+              <p className="text-[#838485] text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </p>
             </div>
           </div>
         </div>
